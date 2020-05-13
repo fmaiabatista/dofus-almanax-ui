a78d91d4b3f64f1f6859c7d5274e8414ad420db6
Felipe Maia <dev.fmaiabatista@gmail.com>
2020-05-12 21:45:43 -0300

    initial commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,48 @@
-import React from "react"
-import { Link } from "gatsby"
+import React, { useState, useEffect } from "react"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
-  <Layout>
+import useInterval from "../hooks/useInterval"
+
+// Ref: https://stackoverflow.com/a/43048463/5046074
+const DUT = () => {
+  const now = new Date();
+  const dut = new Date(+now - now.getTimezoneOffset() + 3600000).toISOString().split(".")[0];
+
+  return <h1>{dut}</h1>;
+}
+
+// Ref: https://overreacted.io/making-setinterval-declarative-with-react-hooks/
+function Counter() {
+  let [count, setCount] = useState(0);
+
+  useInterval(() => {
+    setCount(count + 1);
+  }, 1000);
+
+  return <h1>{count}</h1>;
+}
+
+
+const IndexPage = () => {
+  //Ref: https://www.gatsbyjs.org/docs/data-fetching/#fetching-data-at-client-side-runtime
+  const [starsCount, setStarsCount] = useState(0)
+  useEffect(() => {
+    fetch(`http://worldtimeapi.org/api/timezone/Europe/Paris`)
+      .then(response => response.json())
+      .then(resultData => {
+        setStarsCount(resultData["utc_offset"])
+      })
+  }, [])
+
+  return (<Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
-)
+    <DUT />
+    <Counter />
+    <p>Dofus time: {starsCount}</p>
+    <p>Your current time:</p>
+  </Layout>)
+}
 
 export default IndexPage

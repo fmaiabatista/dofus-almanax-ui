66b58c0ff76224361789f3d31cbed90ad2f0c36b
Felipe Maia <dev.fmaiabatista@gmail.com>
2020-06-16 18:21:25 -0300

    added carousel functionality

--- src/components/gallery.js
+++ src/components/gallery.js
@@ -1,5 +1,9 @@
 import React, { useState } from "react"
 
+import "./gallery.css"
+
+import Card from "./card"
+
 import almanax from "../../content/almanax.json"
 import almanaxB64 from "../../content/almanaxB64.json"
 
@@ -17,30 +21,76 @@ const Gallery = ({ startIndex, lang }) => {
   const { content } = almanax
   const { content: images } = almanaxB64
 
+  const handleAnimation = step => {
+    const c0 = document.querySelector(".c0")
+    const c1 = document.querySelector(".c1")
+    const c2 = document.querySelector(".c2")
+    const c3 = document.querySelector(".c3")
+    const c4 = document.querySelector(".c4")
+
+    if (step === -1) {
+      c0.classList.add("fade-in")
+      c1.classList.add("c1-c2")
+      c2.classList.add("c2-c3")
+      c3.classList.add("fade-out")
+      c4.classList.add("hidden")
+
+      setTimeout(() => {
+        c0.classList.remove("fade-in")
+        c1.classList.remove("c1-c2")
+        c2.classList.remove("c2-c3")
+        c3.classList.remove("fade-out")
+        c4.classList.remove("hidden")
+        setWindow(window.map(el => el + step))
+      }, 450)
+    }
+
+    if (step === 1) {
+      c0.classList.add("hidden")
+      c1.classList.add("fade-out")
+      c2.classList.add("c2-c1")
+      c3.classList.add("c3-c2")
+      c4.classList.add("fade-in")
+
+      setTimeout(() => {
+        c0.classList.remove("hidden")
+        c1.classList.remove("fade-out")
+        c2.classList.remove("c2-c1")
+        c3.classList.remove("c3-c1")
+        c4.classList.remove("fade-in")
+        setWindow(window.map(el => el + step))
+      }, 450)
+    }
+  }
+
   const handleClick = step => {
     if (step === 0) {
+      // Todo - in the future, add animation to go back to "today"
       setWindow(initWindow)
     } else {
-      setWindow(window.map(el => el + step))
+      handleAnimation(step)
     }
   }
 
   return (
-    <div>
-      {window.map(i => (
-        <div>
-          <img
-            src={`data:image/png;base64,${Object.values(images[i])[0]}`}
-            alt={content[i].item.name[lang]}
-            title={content[i].item.name[lang]}
+    <>
+      <div className="gallery">
+        {window.map((i, index) => (
+          <Card
+            key={i}
+            data={{
+              cardIndex: index,
+              info: content[i],
+              image: Object.values(images[i])[0],
+              lang,
+            }}
           />
-          <p>{content[i].merida}</p>
-        </div>
-      ))}
-      <button onClick={() => handleClick(1)}>add</button>
-      <button onClick={() => handleClick(-1)}>sub</button>
-      <button onClick={() => handleClick(0)}>reset</button>
-    </div>
+        ))}
+      </div>
+      <button onClick={() => handleClick(-1)}>-1 day</button>
+      <button onClick={() => handleClick(0)}>Today</button>
+      <button onClick={() => handleClick(1)}>+1 day</button>
+    </>
   )
 }
 

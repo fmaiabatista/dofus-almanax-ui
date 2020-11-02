6bc43b11b8de2cfe56225aca06ba758ee66a4c06
Felipe Maia <dev.fmaiabatista@gmail.com>
2020-11-02 19:26:24 -0300

    added reset icon & testing body height

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import Card from './Card';
 import { ReactComponent as Info } from './assets/info.svg';
 import itemImage from './assets/65759.w75h75.png';
-import { usePalette } from 'react-palette'
+import { usePalette } from 'react-palette';
 
 function App() {
   const today = new Date();
@@ -13,7 +13,7 @@ function App() {
   const { data } = usePalette(itemImage);
 
   return (
-    <div className="App" style={{ background: `linear-gradient(45deg, ${data.vibrant}, ${data.vibrant + '00'})`}}>
+    <div className="App" style={{ background: `linear-gradient(45deg, ${data.vibrant}, ${data.vibrant + '00'})` }}>
       <header className="App-header">
         <span>Dofus Almanax UI</span>
         <div className="App-header-time">
@@ -37,7 +37,16 @@ function App() {
         >
           <path fillRule="evenodd" clipRule="evenodd" d="M18 10.5L40 10.5V5.5L18 5.5L18 0L0 8L18 16V10.5Z" fill={data.darkVibrant} />
         </svg>
-        {isToday && <span className="App-footer-backToToday" onClick={() => setCurrentDay(new Date())} style={{ color: data.darkVibrant, border: `1px solid ${data.darkVibrant}` }}>Back to Today</span>}
+        {isToday && (
+          <svg width="21" height="26" viewBox="0 0 21 26" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path
+              fill-rule="evenodd"
+              clip-rule="evenodd"
+              d="M3.8147e-06 15.6V14.3H2.6V15.6C2.6 19.9017 6.0983 23.4 10.4 23.4C14.7017 23.4 18.2 19.9017 18.2 15.6C18.2 11.2983 14.7017 7.8 10.4 7.8L9.1 7.8V13L2.6 6.5L9.1 0L9.1 5.2L10.4 5.2C16.1343 5.2 20.8 9.8657 20.8 15.6C20.8 21.3343 16.1343 26 10.4 26C4.6657 26 3.8147e-06 21.3343 3.8147e-06 15.6Z"
+              fill={data.darkVibrant}
+            />
+          </svg>
+        )}
         <svg
           onClick={toggleToday}
           className="hover-grow pointer"

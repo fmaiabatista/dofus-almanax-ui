6637a6ea7e6327ac0b6e6ff6b22d2a7888190ca3
Felipe Maia <dev.fmaiabatista@gmail.com>
2020-11-07 20:21:33 -0300

    added color gradient transition

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react';
+import { motion } from 'framer-motion';
 import './App.css';
 import Card from './Card';
 import { ReactComponent as Info } from './assets/info.svg';
@@ -10,10 +11,130 @@ import getDayOfYear from 'date-fns/getDayOfYear';
 import almanax from './data/almanax.json';
 import almanaxImages from './data/almanaxB64.json';
 
+const WHITE = '#FFFFFF';
+
+const blankPalette = {
+  darkMuted: WHITE,
+  darkVibrant: WHITE,
+  lightMuted: WHITE,
+  lightVibrant: WHITE,
+  muted: WHITE,
+  vibrant: WHITE,
+};
+
+// https://css-tricks.com/converting-color-spaces-in-javascript/#hex-to-rgb
+// function hexToRGBA(h = WHITE, alpha = 1) {
+//   let r = 0,
+//     g = 0,
+//     b = 0;
+
+//   if (h.length === 7) {
+//     r = '0x' + h[1] + h[2];
+//     g = '0x' + h[3] + h[4];
+//     b = '0x' + h[5] + h[6];
+//   }
+
+//   return `rgba(${+r}, ${+g}, ${+b}, ${alpha})`;
+// }
+
+function HSLToRGB(hsl) {
+  let sep = hsl.indexOf(",") > -1 ? "," : " ";
+  hsl = hsl.substr(4).split(")")[0].split(sep);
+
+  let h = hsl[0],
+      s = hsl[1].substr(0,hsl[1].length - 1) / 100,
+      l = hsl[2].substr(0,hsl[2].length - 1) / 100;
+
+  // Strip label and convert to degrees (if necessary)
+  if (h.indexOf("deg") > -1)
+  h = h.substr(0,h.length - 3);
+  else if (h.indexOf("rad") > -1)
+  h = Math.round(h.substr(0,h.length - 3) * (180 / Math.PI));
+  else if (h.indexOf("turn") > -1)
+  h = Math.round(h.substr(0,h.length - 4) * 360);
+  // Keep hue fraction of 360 if ending up over
+  if (h >= 360)
+  h %= 360;
+
+  let c = (1 - Math.abs(2 * l - 1)) * s,
+      x = c * (1 - Math.abs((h / 60) % 2 - 1)),
+      m = l - c/2,
+      r = 0,
+      g = 0,
+      b = 0;
+
+  if (0 <= h && h < 60) {
+    r = c; g = x; b = 0;
+  } else if (60 <= h && h < 120) {
+    r = x; g = c; b = 0;
+  } else if (120 <= h && h < 180) {
+    r = 0; g = c; b = x;
+  } else if (180 <= h && h < 240) {
+    r = 0; g = x; b = c;
+  } else if (240 <= h && h < 300) {
+    r = x; g = 0; b = c;
+  } else if (300 <= h && h < 360) {
+    r = c; g = 0; b = x;
+  }
+
+  r = Math.round((r + m) * 255);
+  g = Math.round((g + m) * 255);
+  b = Math.round((b + m) * 255);
+
+  return "rgb(" + r + "," + g + "," + b + ")";
+}
+
+function hexToHSL(H = WHITE, lightness) {
+  // Convert hex to RGB first
+  let r = 0,
+    g = 0,
+    b = 0;
+  if (H.length === 4) {
+    r = '0x' + H[1] + H[1];
+    g = '0x' + H[2] + H[2];
+    b = '0x' + H[3] + H[3];
+  } else if (H.length === 7) {
+    r = '0x' + H[1] + H[2];
+    g = '0x' + H[3] + H[4];
+    b = '0x' + H[5] + H[6];
+  }
+  // Then to HSL
+  r /= 255;
+  g /= 255;
+  b /= 255;
+  let cmin = Math.min(r, g, b),
+    cmax = Math.max(r, g, b),
+    delta = cmax - cmin,
+    h = 0,
+    s = 0,
+    l = 0;
+
+  if (delta === 0) h = 0;
+  else if (cmax === r) h = ((g - b) / delta) % 6;
+  else if (cmax === g) h = (b - r) / delta + 2;
+  else h = (r - g) / delta + 4;
+
+  h = Math.round(h * 60);
+
+  if (h < 0) h += 360;
+
+  l = (cmax + cmin) / 2;
+  s = delta === 0 ? 0 : delta / (1 - Math.abs(2 * l - 1));
+  s = +(s * 100).toFixed(1);
+  l = +(l * 100).toFixed(1);
+
+  return `hsl(${h}, ${s}%, ${lightness || l}%)`;
+}
+
+function hexToHSLToRGB(hex, lightness) {
+  return HSLToRGB(hexToHSL(hex, lightness));
+}
+
 function App() {
   const today = new Date();
   let todaysIndex = getDayOfYear(today) - 1;
   const [currentIndex, setCurrentIndex] = useState(todaysIndex);
+  const [previousPalette, setPreviousPalette] = useState(blankPalette);
   const [currentImageSrc, setCurrentImageSrc] = useState(
     `data:image/jpeg;base64,${almanaxImages.content[currentIndex][almanax.content[currentIndex].date]}`,
   );
@@ -28,11 +149,21 @@ function App() {
   };
 
   useEffect(() => {
+    setPreviousPalette(palette);
     setCurrentImageSrc(`data:image/jpeg;base64,${almanaxImages.content[currentIndex][almanax.content[currentIndex].date]}`);
-  }, [currentIndex]);
+  }, [currentIndex, palette]);
 
   return (
-    <div className="App" style={{ background: `linear-gradient(45deg, ${palette.vibrant}, ${palette.vibrant + '00'})` }}>
+    <motion.div
+      className="App"
+      initial={false}
+      animate={{
+        background: !palette.vibrant
+          ? `linear-gradient(60deg, ${hexToHSLToRGB(previousPalette.vibrant, 40)}, ${hexToHSLToRGB(previousPalette.vibrant, 80)}`
+          : `linear-gradient(60deg, ${hexToHSLToRGB(palette.vibrant, 40)}, ${hexToHSLToRGB(palette.vibrant, 80)})`,
+      }}
+      transition={{ duration: 0.7, ease: 'easeOut' }}
+    >
       <header className="App-header">
         <div className="App-header-logo">
           <picture>
@@ -40,7 +171,7 @@ function App() {
             <img width="32px" src={logoPng} alt="emerald dofus" />
           </picture>
           <span>Dofus Almanax UI</span>
-          {/* <span>{currentIndex}</span> */}
+          {/* {palette.vibrant && <span>{hexToHSL(palette.darkVibrant || previousPalette.darkVibrant)}</span>} */}
         </div>
         <div className="App-header-time">
           <span>{format(today, 'MMM dd yyyy')}</span>
@@ -48,8 +179,7 @@ function App() {
         </div>
       </header>
       <main className="App-main">
-        <img className="App-main-image" src={currentImageSrc} alt={almanax.content[currentIndex].item.name['pt-br']} />
-        <Card item={almanax.content[currentIndex]} imgSrc={currentImageSrc} darkVibrant={palette.darkVibrant} />
+        <Card item={almanax.content[currentIndex]} imgSrc={currentImageSrc} colorAccent={hexToHSLToRGB(palette.darkVibrant, 30)} />
       </main>
       <footer className="App-footer">
         <svg
@@ -61,15 +191,23 @@ function App() {
           fill="none"
           xmlns="http://www.w3.org/2000/svg"
         >
-          <path fillRule="evenodd" clipRule="evenodd" d="M18 10.5L40 10.5V5.5L18 5.5L18 0L0 8L18 16V10.5Z" fill={palette.darkVibrant} />
+          <path fillRule="evenodd" clipRule="evenodd" d="M18 10.5L40 10.5V5.5L18 5.5L18 0L0 8L18 16V10.5Z" fill={hexToHSL(palette.darkVibrant, 20)} />
         </svg>
         {currentIndex !== todaysIndex && (
-          <svg onClick={() => navigate(0)} className="hover-grow pointer" width="26" height="33" viewBox="0 0 26 33" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <svg
+            onClick={() => navigate(0)}
+            className="hover-grow pointer"
+            width="26"
+            height="33"
+            viewBox="0 0 26 33"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
             <path
               fillRule="evenodd"
               clipRule="evenodd"
               d="M3.8147e-06 19.5V17.875H3.25V19.5C3.25 24.8771 7.62288 29.25 13 29.25C18.3771 29.25 22.75 24.8771 22.75 19.5C22.75 14.1229 18.3771 9.75 13 9.75L11.375 9.75L11.375 16.25L3.25 8.125L11.375 0L11.375 6.5H13C20.1679 6.5 26 12.3321 26 19.5C26 26.6679 20.1679 32.5 13 32.5C5.83213 32.5 3.8147e-06 26.6679 3.8147e-06 19.5Z"
-              fill={palette.darkVibrant}
+              fill={hexToHSL(palette.darkVibrant, 20)}
             />
           </svg>
         )}
@@ -82,10 +220,10 @@ function App() {
           fill="none"
           xmlns="http://www.w3.org/2000/svg"
         >
-          <path fillRule="evenodd" clipRule="evenodd" d="M22 5.5H0V10.5H22V16L40 8L22 0V5.5Z" fill={palette.darkVibrant} />
+          <path fillRule="evenodd" clipRule="evenodd" d="M22 5.5H0V10.5H22V16L40 8L22 0V5.5Z" fill={hexToHSL(palette.darkVibrant, 20)} />
         </svg>
       </footer>
-    </div>
+    </motion.div>
   );
 }
 
--- src/Card.js
+++ src/Card.js
@@ -1,27 +1,31 @@
 import './Card.css';
+import { motion } from 'framer-motion';
 import { ReactComponent as Encyclopedia } from './assets/encyclopedia.svg';
 import { ReactComponent as Copy } from './assets/copy.svg';
 import { format } from 'date-fns';
 
-function Card({ item, imgSrc, darkVibrant }) {
+function Card({ item, imgSrc, colorAccent }) {
   const date = new Date(`${item.date}T12:00:00`);
   const openEncyclopedia = (link) => {
     window.open(link, '_blank');
   };
 
   return (
-    <div className="Card" style={{ boxShadow: `0px 0px 12px 2px ${darkVibrant}` }}>
+    <motion.div className="Card" initial={false} animate={{ color: colorAccent }} transition={{ duration: 0.7, ease: 'easeOut' }}>
       <main>
         <img className="Card-main-image" src={imgSrc} alt="itemImageAlt" />
-        <span className="Card-main-title">{item.item.name['pt-br']}</span>
+        <span className="Card-main-title">{item.item.name['pt-br'] || `${item.item.name['en-us']} *`}</span>
         <span className="Card-main-quantity">x{item.item.qty}</span>
       </main>
       <footer>
-        <Encyclopedia className="hover-grow pointer" onClick={() => openEncyclopedia(item.item.link['pt-br'])} />
+        <Encyclopedia
+          className="hover-grow pointer"
+          onClick={() => openEncyclopedia(item.item.link['pt-br'] || item.item.link['en-us'] || item.item.link['search'])}
+        />
         <span className="Card-footer-date">{format(date, 'MMM dd yyyy')}</span>
         <Copy className="hover-grow pointer" />
       </footer>
-    </div>
+    </motion.div>
   );
 }
 
